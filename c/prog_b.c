df92d303621a5673a309c81e26a7b8ef32643c95
Nahuel Defossé <nahuel.defosse@gmail.com>
2016-03-18 13:58:58 -0300

    Agregar el nombre del proceso y un ID.

---
+++ c/prog_b.c
@@ -0,0 +1,28 @@
+#include "comm.h"
+#include <string.h> // strncpy
+#include <unistd.h>
+
+
+int main(int argc, char **argv)
+{
+    TMensaje m;
+    int count = 10;
+
+    iniciar(argc, argv);
+
+    while(count-- > 0)
+    {
+        strncpy(m.msg, "Hola que tal", 80);
+        // Enviar el mensaje
+        m.x = count;
+        m.y = count +1;
+        m.pid = getpid();
+        enviar(&m);
+        usleep(500000);
+    }
+    // Avisa a Processing que se va
+    m.estado = -1;
+    enviar(&m);
+
+    return 0;
+}
