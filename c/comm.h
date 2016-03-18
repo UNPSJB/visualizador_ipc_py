183f90c6e6883d63693981daa6a10c5ba3e963bd
Nahuel Defossé <nahuel.defosse@gmail.com>
2016-03-18 14:21:02 -0300

    Actualización del .h

--- c/comm.h
+++ c/comm.h
@@ -6,8 +6,11 @@
  * Modificar esta estructura para mostrar información útil
  */
 typedef struct {
+    char prog_name[20];
     int pid;
     int id;
+    int estado;
+    /* Información extra */
     char msg[80];
     int x;
     int y;
@@ -19,7 +22,7 @@ typedef struct {
  * Iniciar la comunicación con Processing.
  * Debe ser llamado antes de utilizar enviar.
  */
-void iniciar(int argc, char **argv);
+void iniciar(int argc, char **argv, TMensaje *m);
 /**
  * Enviar un mensaje a Processing
  * @param m mensaje a ser enviado. Este mensaje está definido en el tipo TMensaje que
